049aa9c70d35ce9ede6682fba3681a3faad2c828
Armando Perez <armandopmj@gmail.com>
2014-07-10 15:30:16 -0700

    Finished stingifyJSON.js

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -3,5 +3,32 @@
 
 // but you don't so you're going to write it from scratch:
 var stringifyJSON = function(obj) {
-  // your code goes here
+
+	var t = typeof obj, result = "";
+	
+	if( t === "number" || t === "boolean" ) return String(obj);
+	if( obj === undefined || obj === "function") return;
+	if( obj === null ) return "null";
+		
+	if( t === "string" ) { 
+		return (new String('"') + new String( obj ) + new String('"')).toString();
+	}
+
+	if( Object.prototype.toString.call( obj ) === '[object Array]' ){
+		for(var i = 0; i < obj.length; i++) {
+			result += stringifyJSON( obj[i] );
+			if( i < obj.length - 1 ) result += ",";
+		}
+		return "["+ result + "]"
+	}
+
+	if( Object.prototype.toString.call( obj ) === '[object Object]' ){
+	    for (var p in obj) {
+	        if (obj.hasOwnProperty(p) && stringifyJSON( obj[p] ) !== undefined ) {
+	        	result += stringifyJSON( p ) +':'+ stringifyJSON( obj[p] ) +",";
+	        }
+	    }
+	    if(result === "") return "{}";
+	    else return "{"+ result.slice(0, -1) +"}";
+	}
 };
